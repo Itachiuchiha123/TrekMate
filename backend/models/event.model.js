d52559cc6032ac3ad3e52f2deb6d6d60e7057069
Itachiuchiha123 <nishantjais541@gmail.com>
2024-11-20 01:54:46 +0545

    Create basic routes for hosting and joining events

--- backend/models/event.model.js
+++ backend/models/event.model.js
@@ -3,6 +3,12 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const eventSchema = new mongoose.Schema(
     {
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+            maxLength: [32, "Title's max length hit"],
+        },
         // TODO make location something that actually represents a location?
         location: {
             type: String, // For now
@@ -19,12 +25,14 @@ const eventSchema = new mongoose.Schema(
         },
         members: [
             {
-                user: { type: ObjectId, ref: "User" },
+                user: { type: ObjectId, ref: "User", required: true },
                 role: {
                     type: String,
                     enum: ["host", "admin", "member"],
+                    default: "member",
                     required: true,
                 },
+                joined_at: { type: Date, default: Date.now },
             },
         ],
         invites: [
@@ -35,6 +43,7 @@ const eventSchema = new mongoose.Schema(
                     type: String,
                     enum: ["pending", "accepted", "rejected"],
                     default: "pending",
+                    required: true,
                 },
             },
         ],
